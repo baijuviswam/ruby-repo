1472d0aa4225b712f55a503e30b1bf25debbe6b5
Baijumon Viswambharan <baiju.k.viswam@gmail.com>
2021-07-04 12:49:34 -0700

    added release version

---
+++ welcome_1.0.rb
@@ -0,0 +1,9 @@
+puts "The releae version is 1.0"
+
+puts "Hello, what's your name ?"
+name = gets.chomp 
+
+puts "What's your age ?"
+age = gets.chomp
+
+puts "Hello #{name}. Welcome to ruby repo !!, You are #{age} years older !!"
